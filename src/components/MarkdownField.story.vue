c0c5ddd642aa045fb8079ab8d575597121017201
Linton <linton@onesheep.org>
2022-11-25 07:53:58 +0200

    chore: include documentation

--- src/components/MarkdownField.story.vue
+++ src/components/MarkdownField.story.vue
@@ -1,5 +1,5 @@
 <template>
-  <Story title="Markdown Field">
+  <Story title="Markdown Field" id="markdown-field">
     <Variant title="Empty">
       <MarkdownField
         :field="{
@@ -54,7 +54,36 @@
     </Variant>
   </Story>
 </template>
+<docs lang="md">
+# Markdown Field
 
+## Props
+
+**modelValue**
+This is the model value
+
+type: String,
+required: true
+
+**field**
+Data for this field
+type: Object as PropType<FieldSpec>
+required: true,
+
+**error**
+The error message
+type: Object,
+required: false,
+
+**isNested**
+Indicates whether this is a nested widget or not
+type: Boolean,
+default: false,
+
+## Emits
+
+modelValue is emited on the Easy Markdown Editor change
+</docs>
 <script setup>
 import MarkdownField from "./MarkdownField.vue";
 </script>
--- src/components/StringField.story.vue
+++ src/components/StringField.story.vue
@@ -54,7 +54,36 @@
     </Variant>
   </Story>
 </template>
+<docs lang="md">
+# String Field
 
+## Props
+
+**modelValue**
+This is the model value
+
+type: String,
+required: true
+
+**field**
+Data for this field
+type: Object as PropType<FieldSpec>
+required: true,
+
+**error**
+The error message
+type: Object,
+required: false,
+
+**isNested**
+Indicates whether this is a nested widget or not
+type: Boolean,
+default: false,
+
+## Emits
+
+modelValue is emited on string field input
+</docs>
 <script setup>
 import StringField from "./StringField.vue";
 </script>
