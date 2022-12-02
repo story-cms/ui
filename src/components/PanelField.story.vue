df63bf7bc9196fdd899973ba8073271d5856aafe
Paurakh Sharma Humagain <paurakh011@gmail.com>
2022-12-02 14:25:39 +0545

    fix: PanelField story

--- src/components/PanelField.story.vue
+++ src/components/PanelField.story.vue
@@ -1,8 +1,22 @@
 <template>
   <Story title="PanelField">
     <Variant title="Default">
-      <PanelField name="intro" label="Intro" widget="panel" isReadonly: false,
-      :field="spec" :model="model" />
+      <PanelField :field="spec" :form="model" />
+      <code>
+        <pre class="text-gray-600">{{ JSON.stringify(model, null, 2) }}</pre>
+      </code>
+    </Variant>
+    <Variant title="Readonly Reference">
+      <PanelField
+        :field="{ ...spec, fields: readOnlyReferenceFields }"
+        :form="model"
+      />
+      <code>
+        <pre class="text-gray-600">{{ JSON.stringify(model, null, 2) }}</pre>
+      </code>
+    </Variant>
+    <Variant title="Error">
+      <PanelField :field="spec" :form="errorModel" :errors="errors" />
       <code>
         <pre class="text-gray-600">{{ JSON.stringify(model, null, 2) }}</pre>
       </code>
@@ -30,16 +44,39 @@ const fields: FieldSpec[] = [
   },
 ];
 
+const readOnlyReferenceFields: FieldSpec[] = [
+  {
+    label: "Reference",
+    name: "reference",
+    widget: "string",
+    isReadonly: true,
+  },
+  {
+    label: "NIV",
+    name: "quote",
+    widget: "markdown",
+    isReadonly: false,
+  },
+];
+
 const spec = {
   label: "Sections",
   name: "sections",
-  widget: "list",
+  widget: "panel",
   isReadonly: false,
   fields: fields,
 };
 
+const errors = { "bundle.reference": ["bad!"] };
+
 const model = ref({
   reference: "John 1:14",
+  quote:
+    "We have seen his glory, the glory of the one and only Son, who came from the Father, full of grace and truth.",
+});
+
+const errorModel = ref({
+  reference: "",
   quote:
     "We have seen his glory, the glory of the one and only Son, who came from the Father, full of grace and truth.",
 });
