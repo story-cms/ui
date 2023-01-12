1152eee48192e443dec8f6b12677ceb533537e39
Timothy Koech <timosville@gmail.com>
2023-01-12 13:09:46 +0300

    chore: change case

--- src/Draft/SelectField.vue
+++ src/Draft/SelectField.vue
@@ -29,7 +29,7 @@
 import { computed, ref, nextTick, onMounted } from 'vue';
 import { FieldSpec } from 'App/Models/Interfaces';
 import { useLanguageStore, useModelStore } from '../store';
-import { commonProps } from '../Shared/helpers';
+import { commonProps } from '../shared/helpers';
 
 const props = defineProps({
   ...commonProps,
