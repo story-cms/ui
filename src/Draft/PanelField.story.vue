c20a3a0ce88dbde41b299eb5801e69e25741cd79
Linton <linton@onesheep.org>
2023-01-17 12:06:42 +0200

    added capsule label control

--- src/Draft/PanelField.story.vue
+++ src/Draft/PanelField.story.vue
@@ -16,54 +16,76 @@
       <PanelField :field="{ ...spec, fields: readOnlyTitle }" />
       <ModelControl :model="panelModel" />
     </Variant>
+
+    <Variant title="No Label" :setup-app="loadData">
+      <PanelField :field="noLabel" />
+      <ModelControl :model="panelModel" />
+    </Variant>
   </Story>
 </template>
 
 <script lang="ts" setup>
-import PanelField from "./PanelField.vue";
-import ModelControl from "../helpers/ModelControl.vue";
-import ErrorControl from "../helpers/ErrorControl.vue";
-import { panelErrors, panelModel } from "../helpers/mocks";
-import type { Vue3StorySetupHandler } from "@histoire/plugin-vue";
-import { useModelStore } from "../store";
+import PanelField from './PanelField.vue';
+import ModelControl from '../helpers/ModelControl.vue';
+import ErrorControl from '../helpers/ErrorControl.vue';
+import { panelErrors, panelModel } from '../helpers/mocks';
+import type { Vue3StorySetupHandler } from '@histoire/plugin-vue';
+import { useModelStore } from '../store';
 
 const loadData: Vue3StorySetupHandler = ({ app, story, variant }) => {
   const store = useModelStore();
   store.model = panelModel;
-  if (variant?.title == "Error") {
+  if (variant?.title == 'Error') {
     store.errors = panelErrors;
   }
 };
 
 const spec = {
-  label: "Note",
-  name: "note",
-  widget: "panel",
+  label: 'Note',
+  name: 'note',
+  widget: 'panel',
   fields: [
     {
-      label: "Title",
-      name: "title",
-      widget: "string",
+      label: 'Title',
+      name: 'title',
+      widget: 'string',
     },
     {
-      label: "Description",
-      name: "description",
-      widget: "markdown",
+      label: 'Description',
+      name: 'description',
+      widget: 'markdown',
     },
   ],
 };
 
 const readOnlyTitle = [
   {
-    label: "Title",
-    name: "title",
-    widget: "string",
+    label: 'Title',
+    name: 'title',
+    widget: 'string',
     isReadOnly: true,
   },
   {
-    label: "Description",
-    name: "description",
-    widget: "markdown",
+    label: 'Description',
+    name: 'description',
+    widget: 'markdown',
   },
 ];
+const noLabel = {
+  label: '',
+  name: 'note',
+  widget: 'panel',
+  fields: [
+    {
+      label: 'Title',
+      name: 'title',
+      widget: 'string',
+    },
+    {
+      label: 'Description',
+      name: 'description',
+      widget: 'markdown',
+    },
+  ],
+};
 </script>
--- src/Draft/PanelField.vue
+++ src/Draft/PanelField.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="rounded border border-gray-100 bg-white p-[32px] shadow-sm">
+    <div class="relative flex justify-center">
+      <button
+        v-if="field.label && field.label.trim() !== ''"
+        type="button"
+        class="inline-flex items-center rounded-full border border-gray-300 bg-white px-4 py-1.5 text-sm font-medium leading-5 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        <span>{{ field.label }}</span>
+      </button>
+    </div>
+
     <div v-for="(item, index) in fields" :key="index">
       <component
         :is="widgetFor(index)"
@@ -12,17 +22,16 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import { commonProps } from "../shared/helpers";
-import { FieldSpec } from "App/Models/Interfaces";
-import { widgetField } from "./widget-fields";
+import { computed, ref } from 'vue';
+import { commonProps } from '../shared/helpers';
+import { FieldSpec } from 'App/Models/Interfaces';
+import { widgetField } from './widget-fields';
 
 const props = defineProps({
   ...commonProps,
 });
 
 const field = computed(() => props.field as FieldSpec);
-
 const fields = field.value.fields as FieldSpec[];
 
 const widgetFor = (key: number) => {
