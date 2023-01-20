25f94a8c2a65b937c8abc45498c093bbc5e17591
Timothy Koech <timosville@gmail.com>
2023-01-20 10:28:19 +0300

    chore: introduce error

--- src/Shared/helpers.ts
+++ src/Shared/helpers.ts
@@ -49,7 +49,7 @@ export const parseReference = (reference: string): string => {
   if (bookNum) {
     book = `${bookNum} ${book}`;
   }
-  const abbreviation = getAbbreviation(book);
+  let abbreviation = getAbbreviation(book);
 
   if (!abbreviation) {
     return '';
