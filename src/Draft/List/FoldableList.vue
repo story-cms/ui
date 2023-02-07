06cb1f2b90e60b7410a91dc895acb97487d5aec5
Timothy Koech <timosville@gmail.com>
2023-02-07 13:20:04 +0300

    refactor: add closing tag

--- src/Draft/List/FoldableList.vue
+++ src/Draft/List/FoldableList.vue
@@ -3,7 +3,7 @@
     <div v-for="(_listItem, index) in listItems" :key="index" class="relative">
       <div class="relative">
         <div class="absolute inset-0 flex items-center" aria-hidden="true">
-          <div class="w-full border-t border-gray-300" />
+          <div class="w-full border-t border-gray-300"></div>
         </div>
         <div class="relative flex justify-between">
           <button
