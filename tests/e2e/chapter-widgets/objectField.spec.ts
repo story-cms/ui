280c110f797efb4d924596024603744f1c8bdf1f
Timothy Koech <timosville@gmail.com>
2023-05-10 10:18:54 +0300

    wip: object field

---
+++ tests/e2e/chapter-widgets/objectField.spec.ts
@@ -0,0 +1,81 @@
+import { test, expect } from '@playwright/test';
+import { objectSpec, objectModel } from '@/helpers/mocks';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/src-draft-objectfield-story-vue');
+  await page.getByRole('link', { name: 'ObjectField 9' }).click();
+});
+test.afterEach(async ({ page }) => {
+  await page.close();
+});
+
+test.describe('Object Field', () => {
+  test('should show default fields', async ({ page }) => {
+    await page.getByRole('link', { name: 'Default' }).click();
+
+    for (const item of objectSpec.fields) {
+      if (item.widget === 'string') {
+        await expect(
+          page
+            .frameLocator('[data-test-id="preview-iframe"]')
+            .locator(`input[name=${item.label}]`),
+        ).toHaveValue(objectModel.address[item.name]);
+      }
+      if (item.widget === 'scripture') {
+        await expect(
+          page
+            .frameLocator('[data-test-id="preview-iframe"]')
+            .getByPlaceholder('John 1 or John 1:3-4'),
+        ).toHaveValue(objectModel.address.favoriteScripture.reference);
+        await expect(
+          page.frameLocator('[data-test-id="preview-iframe"]').getByPlaceholder('Verse'),
+        ).toHaveValue(objectModel.address.favoriteScripture.verse);
+      }
+    }
+  });
+  test('should make fields readonly', async ({ page }) => {
+    await page.getByRole('link', { name: 'Readonly' }).click();
+
+    for (const item of objectSpec.fields) {
+      if (item.widget === 'string') {
+        await expect(
+          page
+            .frameLocator('[data-test-id="preview-iframe"]')
+            .locator(`input[name=${item.label}]`),
+        ).toHaveValue(objectModel.address[item.name]);
+        await expect(
+          page
+            .frameLocator('[data-test-id="preview-iframe"]')
+            .locator(`input[name=${item.label}]`),
+        ).toHaveAttribute('readonly', '');
+      }
+      if (item.widget === 'scripture') {
+        await expect(
+          page
+            .frameLocator('[data-test-id="preview-iframe"]')
+            .getByPlaceholder('John 1 or John 1:3-4'),
+        ).toHaveValue(objectModel.address.favoriteScripture.reference);
+        await expect(
+          page.frameLocator('[data-test-id="preview-iframe"]').getByPlaceholder('Verse'),
+        ).toHaveValue(objectModel.address.favoriteScripture.verse);
+        await expect(
+          page
+            .frameLocator('[data-test-id="preview-iframe"]')
+            .getByPlaceholder('John 1 or John 1:3-4'),
+        ).toHaveAttribute('readonly', '');
+        await expect(
+          page.frameLocator('[data-test-id="preview-iframe"]').getByPlaceholder('Verse'),
+        ).toHaveAttribute('readonly', '');
+      }
+    }
+  });
+  test('should show error message', async ({ page }) => {
+    await page.getByRole('link', { name: 'Error', exact: true }).click();
+
+    await expect(
+      page
+        .frameLocator('[data-test-id="preview-iframe"]')
+        .getByText('required validation failed'),
+    ).toBeVisible();
+  });
+});
