2906a3b0073d7e47a22e8e5819717ea9a82b43e4
Linton <linton@onesheep.org>
2023-06-07 09:47:05 +0200

    feat: add drop down shared widget story

---
+++ src/Shared/DropDown.story.vue
@@ -0,0 +1,20 @@
+<template>
+  <Story title="Drop Down" group="shared">
+    <Variant title="With options">
+      <DropDown :options="options" />
+    </Variant>
+    <Variant title="With value">
+      <DropDown :options="options" :isReadOnly="isReadOnly" modelValue="Add" />
+    </Variant>
+    <Variant title="Read only">
+      <DropDown :options="options" :isReadOnly="isReadOnly" />
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import DropDown from './DropDown.vue';
+
+const options = ['Add', 'Edit', 'Delete'];
+const isReadOnly = true;
+</script>
