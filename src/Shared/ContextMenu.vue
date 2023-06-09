2ecd075a7402d8340a253574eedfb5a21bca942c
Jannie Theunissen <jannie@onesheep.org>
2023-06-09 17:36:17 +0200

    refactor: package tweaks

--- src/Shared/ContextMenu.vue
+++ src/Shared/ContextMenu.vue
@@ -4,7 +4,7 @@
       <MenuButton
         class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       >
-        Edition
+        {{ anchor }}
         <Icon class="h-5 w-5 text-gray-400" aria-hidden="true" name="chevron-down" />
       </MenuButton>
     </div>
@@ -48,6 +48,10 @@ import Icon from '../Shared/Icon.vue';
 defineProps({
   options: {
     type: Array as PropType<string[]>,
+    required: true,
+  },
+  anchor: {
+    type: String,
     required: true,
   },
 });
