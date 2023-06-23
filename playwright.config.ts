3f2495fd1dfd6a8f90c912a7d3775551e66de673
Timothy Koech <timosville@gmail.com>
2023-06-23 14:59:09 +0300

    chore: test deploy 4

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://127.0.0.1:6006',
+    url: 'http://localhost:6006',
     reuseExistingServer: !process.env.CI,
   },
 });
