244bc3786d8fcf7e0bcd973b895fc5ae5175c15f
Timothy Koech <timosville@gmail.com>
2023-07-14 14:08:15 +0300

    chore: refactor  url

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   ],
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:6006',
-    reuseExistingServer: !process.env.CI,
+    url: 'http://127.0.0.1:6006/story',
+    reuseExistingServer: true,
   },
 });
