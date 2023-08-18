31dd2b62556c2f160eb1fc9a425010f30726b1b0
Timothy Koech <timosville@gmail.com>
2023-08-18 08:55:17 +0300

    chore: package

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,6 @@ export default defineConfig({
         '@aws-sdk/client-s3',
         '@aws-sdk/lib-storage',
         '@rive-app/canva',
-        '@inertiajs',
         'axios',
         'easymde',
         'luxon',
