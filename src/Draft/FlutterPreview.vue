2f9b6f597be7d3982372fb6d9452619dc3820c4a
Timothy Koech <timosville@gmail.com>
2023-10-03 15:15:36 +0300

    feat: add episode to the flutter preview

--- src/Draft/FlutterPreview.vue
+++ src/Draft/FlutterPreview.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Flutter preview</h1>
+  <div ref="flutterTarget"></div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted, PropType } from 'vue';
+
+const flutterTarget = ref(null);
+const props = defineProps({
+  loadEpisode: Function as PropType<() => void>,
+});
+
+onMounted(() => {
+  // Embed Flutter into the div with ref "flutterTarget"
+  const target = flutterTarget.value;
+
+  _flutter.loader.loadEntrypoint({
+    onEntrypointLoaded: async (engineInitializer: any) => {
+      const appRunner = await engineInitializer.initializeEngine({
+        hostElement: target,
+        assetBase: '/web/',
+      });
+      await appRunner.runApp();
+      setTimeout(() => {
+        props.loadEpisode();
+      }, 1000);
+    },
+  });
+});
+</script>
