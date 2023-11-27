5999e3f6bd47364738909e19b39afc8dde064dfb
Timothy Koech <timosville@gmail.com>
2023-11-27 08:33:42 +0300

    wip: toggle metabox and app preview

--- src/Draft/DraftIndex.vue
+++ src/Draft/DraftIndex.vue
@@ -8,52 +8,52 @@
     >
       <WorkflowButtons @request-change="reject" @publish="publish" @submit="submit" />
     </ContentHeader>
-    <div class="container mx-auto mb-32 pb-4">
-      <div class="flex justify-between space-x-8">
-        <div class="w-max flex-grow overflow-hidden rounded-sm lg:max-w-[800px]">
-          <form class="space-y-8">
-            <div v-for="(item, index) in drafts.story.fields" :key="index">
-              <component :is="widgetFor(index)" :field="item" :is-nested="false" />
-            </div>
-          </form>
+
+    <div class="container relative mx-auto lg:grid lg:grid-cols-[1fr_416px] lg:gap-x-9">
+      <form class="space-y-8">
+        <div v-for="(item, index) in drafts.story.fields" :key="index">
+          <component :is="widgetFor(index)" :field="item" :is-nested="false" />
         </div>
-        <div class="sticky top-0 h-full w-[416px]">
-          <section v-if="showMetaBox">
-            <MetaBox
-              :created-at="props.draft.createdAt"
-              :updated-at="props.draft.updatedAt"
-              :story-type="props.meta.storyType"
-              :chapter-type="metaChapter"
-              :published-when="published_when"
-              @close="showMetaBox = false"
+      </form>
+
+      <div class="absolute right-2 top-2">
+        <section v-if="showMetaBox">
+          <MetaBox
+            :created-at="props.draft.createdAt"
+            :updated-at="props.draft.updatedAt"
+            :story-type="props.meta.storyType"
+            :chapter-type="metaChapter"
+            :published-when="published_when"
+            :is-floating="!isLargeScreen"
+            @close="showMetaBox = false"
+          />
+        </section>
+
+        <section v-if="showAppPreview" class="mt-6">
+          <MobileAppPreview
+            :is-floating="!isLargeScreen"
+            :bundle="bundle"
+            class="mt-2"
+            @close="closeS"
+          />
+        </section>
+        <div
+          v-if="feedbackPanel.message"
+          class="mt-[24px] overflow-hidden rounded-sm bg-white shadow"
+        >
+          <div class="flex items-center px-5 py-6">
+            <Icon
+              v-if="feedbackPanel.icon"
+              :name="feedbackPanel.icon"
+              class="mr-[18px]"
+              :class="{
+                'text-red-500': feedbackPanel.icon == 'exclamation',
+                'text-blue-500': feedbackPanel.icon == 'check',
+                'text-green-500': feedbackPanel.icon == 'check-badge',
+                'text-yellow-500': feedbackPanel.icon == 'exclamation-circle',
+              }"
             />
-          </section>
-          <section v-if="showAppPreview" class="mt-6">
-            <p class="text-center font-['Inter'] text-lg/7 font-medium">Draft Preview</p>
-            <MobileAppPreview
-              v-if="shared.meta.hasAppPreview"
-              :bundle="bundle"
-              class="mt-2"
-            />
-          </section>
-          <div
-            v-if="feedbackPanel.message"
-            class="mt-[24px] overflow-hidden rounded-sm bg-white shadow"
-          >
-            <div class="flex items-center px-5 py-6">
-              <Icon
-                v-if="feedbackPanel.icon"
-                :name="feedbackPanel.icon"
-                class="mr-[18px]"
-                :class="{
-                  'text-red-500': feedbackPanel.icon == 'exclamation',
-                  'text-blue-500': feedbackPanel.icon == 'check',
-                  'text-green-500': feedbackPanel.icon == 'check-badge',
-                  'text-yellow-500': feedbackPanel.icon == 'exclamation-circle',
-                }"
-              />
-              <p class="text-sm font-medium leading-4">{{ feedbackPanel.message }}</p>
-            </div>
+            <p class="text-sm font-medium leading-4">{{ feedbackPanel.message }}</p>
           </div>
         </div>
       </div>
@@ -62,7 +62,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, onMounted } from 'vue';
+import { computed, ref, onMounted, onUnmounted } from 'vue';
 import AppLayout from '../Shared/AppLayout.vue';
 import ContentHeader from '../Shared/ContentHeader.vue';
 import MetaBox from '../Shared/MetaBox.vue';
@@ -75,6 +75,10 @@ import MobileAppPreview from './MobileAppPreview.vue';
 import WorkflowButtons from '../Draft/WorkflowButtons.vue';
 
 const props = defineProps<DraftEditProps & SharedPageProps>();
+
+const closeS = () => {
+  showAppPreview.value = false;
+};
 
 const model = useModelStore();
 model.setFromProps(props);
@@ -169,12 +173,23 @@ const reject = () => {
 
 const showMetaBox = ref(false);
 const showAppPreview = ref(false);
+const isLargeScreen = ref(false);
+const windowWidth = ref(window.innerWidth);
+
+const handleResize = () => {
+  windowWidth.value = window.innerWidth;
+  windowWidth.value >= 1024
+    ? (isLargeScreen.value = true)
+    : (isLargeScreen.value = false);
+};
 
 const info = () => {
+  if (isLargeScreen.value) return;
   showMetaBox.value = !showMetaBox.value;
 };
 
 const appPreview = () => {
+  if (isLargeScreen.value) return;
   showAppPreview.value = !showAppPreview.value;
 };
 const published_when = computed(() => {
@@ -195,10 +210,15 @@ onMounted(() => {
     save();
     chapterTitle.value = model.getField('title', '') || defaultTitle.value;
   });
+  window.addEventListener('resize', handleResize);
 });
 
 const widgetFor = (key: number) => {
   const widget = (props.spec.fields as FieldSpec[])[key].widget;
   return widgets.picker(widget);
 };
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
 </script>
