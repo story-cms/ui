385d5d365e3ca362dda48d8ead14f1841189a222
Timothy Koech <timosville@gmail.com>
2023-11-27 08:32:26 +0300

    refactor: add close button

--- src/Draft/MobileAppPreview.vue
+++ src/Draft/MobileAppPreview.vue
@@ -1,12 +1,20 @@
 <template>
   <div
-    class="grid h-[844px] w-[407px] place-content-center bg-[url('data:image/svg+xml,%3Csvg%20width%3D%22407%22%20height%3D%22844%22%20viewBox%3D%220%200%20407%20844%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0A%3Crect%20width%3D%22375%22%20height%3D%22812%22%20transform%3D%22translate(16%2016)%22%20fill%3D%22%23EBE9DE%22%2F%3E%0A%3Cpath%20d%3D%22M99%2018.8408H309V30.6564C309%2041.7021%20300.046%2050.6564%20289%2050.6564H119C107.954%2050.6564%2099%2041.7021%2099%2030.6564V18.8408Z%22%20fill%3D%22black%22%2F%3E%0A%3Crect%20x%3D%229.5%22%20y%3D%229.5%22%20width%3D%22388%22%20height%3D%22825%22%20rx%3D%2258.5%22%20stroke%3D%22%23111827%22%20stroke-width%3D%2219%22%2F%3E%0A%3C%2Fsvg%3E%0A')]"
+    class="relative grid h-[844px] w-[407px] place-content-center bg-[url('data:image/svg+xml,%3Csvg%20width%3D%22407%22%20height%3D%22844%22%20viewBox%3D%220%200%20407%20844%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0A%3Crect%20width%3D%22375%22%20height%3D%22812%22%20transform%3D%22translate(16%2016)%22%20fill%3D%22%23EBE9DE%22%2F%3E%0A%3Cpath%20d%3D%22M99%2018.8408H309V30.6564C309%2041.7021%20300.046%2050.6564%20289%2050.6564H119C107.954%2050.6564%2099%2041.7021%2099%2030.6564V18.8408Z%22%20fill%3D%22black%22%2F%3E%0A%3Crect%20x%3D%229.5%22%20y%3D%229.5%22%20width%3D%22388%22%20height%3D%22825%22%20rx%3D%2258.5%22%20stroke%3D%22%23111827%22%20stroke-width%3D%2219%22%2F%3E%0A%3C%2Fsvg%3E%0A')]"
   >
+    <button
+      v-if="props.isFloating"
+      class="absolute -right-2 -top-2 inline-flex h-[42px] w-[42px] items-center justify-center rounded-full bg-white"
+      @click.prevent="emit('close')"
+    >
+      <icon name="cross" class="h-6 w-6" />
+    </button>
     <div ref="flutterTarget" class="h-[730px] w-[350px] border transition-all"></div>
   </div>
 </template>
 <script setup lang="ts">
 import { ref, onMounted, watch } from 'vue';
+import Icon from '../Shared/Icon.vue';
 
 declare global {
   interface Window {
@@ -16,10 +24,15 @@ declare global {
 }
 
 const flutterTarget = ref(null);
+
 const props = defineProps({
   bundle: {
     type: Object,
     required: true,
+  },
+  isFloating: {
+    type: Boolean,
+    default: false,
   },
 });
 
@@ -57,6 +70,8 @@ const flutterLoader = () => {
     },
   });
 };
+
+const emit = defineEmits(['close']);
 
 onMounted(() => {
   flutterLoader();
