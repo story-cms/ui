76f11e622590472137ed58db6ccbb803340cc99b
Timothy Koech <timosville@gmail.com>
2023-11-28 08:51:36 +0300

    feat: add message centre

---
+++ src/Shared/MessageCentre.story.vue
@@ -0,0 +1,33 @@
+<template>
+  <Story title="Message" group="shared">
+    <Variant title="Failure">
+      <MessageCentre
+        :response="ResponseStatus.Failure"
+        :message="'Oh no, you broke the internet'"
+      />
+    </Variant>
+    <Variant title="Confirmation">
+      <MessageCentre
+        :response="ResponseStatus.Confirmation"
+        :message="'Yes this was done'"
+      />
+    </Variant>
+    <Variant title="Accomplishment">
+      <MessageCentre
+        :response="ResponseStatus.Accomplishment"
+        :message="'You achieved a thing!'"
+      />
+    </Variant>
+    <Variant title="Neutral">
+      <MessageCentre
+        :response="ResponseStatus.Neutral"
+        :message="'Neutral feedback point'"
+      />
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import MessageCentre from './MessageCentre.vue';
+import { ResponseStatus } from './interfaces';
+</script>
---
+++ src/Shared/MessageCentre.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="[&>span]:justify-center] flex h-24 w-full items-center justify-center space-x-6 bg-white [&>span]:flex [&>span]:items-center [&>span]:space-x-6"
+  >
+    <span v-if="response === 0">
+      <Icon class="text-red-500" :name="'exclamation'" />
+      <span>{{ message }}</span>
+    </span>
+    <span v-if="response === 1">
+      <Icon class="text-red-500" :name="'check'" />
+      <span>{{ message }}</span>
+    </span>
+    <span v-if="response === 2">
+      <Icon class="text-green-500" :name="'badge-check'" />
+      <span>{{ message }}</span>
+    </span>
+    <span v-if="response === 3">
+      <Icon class="text-yellow-500" :name="'information-circle'" />
+      <span>{{ message }}</span>
+    </span>
+  </div>
+</template>
+<script setup lang="ts">
+import Icon from './Icon.vue';
+import { ResponseStatus } from './interfaces';
+
+defineProps<{
+  response: ResponseStatus;
+  message: string;
+}>();
+</script>
