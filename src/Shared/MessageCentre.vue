3d85c3664bc4916eb3ca42d173f57997cec865a8
Timothy Koech <timosville@gmail.com>
2023-11-28 09:24:29 +0300

    refactor: centre icons

--- src/Shared/MessageCentre.vue
+++ src/Shared/MessageCentre.vue
@@ -1,21 +1,30 @@
 <template>
   <div
-    class="[&>span]:justify-center] flex h-24 w-full items-center justify-center space-x-6 bg-white [&>span]:flex [&>span]:items-center [&>span]:space-x-6"
+    class="flex h-24 w-full items-center justify-center space-x-6 bg-white [&>span]:flex [&>span]:items-center [&>span]:justify-center [&>span]:space-x-6 [&>span]:text-sm/4 [&>span]:font-medium"
   >
     <span v-if="response === 0">
-      <Icon class="text-red-500" :name="'exclamation'" />
+      <Icon
+        class="inline-flex h-6 w-auto items-center text-red-500"
+        :name="'exclamation'"
+      />
       <span>{{ message }}</span>
     </span>
     <span v-if="response === 1">
-      <Icon class="text-red-500" :name="'check'" />
+      <Icon class="inline-flex h-6 w-6 items-center text-blue-500" :name="'check'" />
       <span>{{ message }}</span>
     </span>
     <span v-if="response === 2">
-      <Icon class="text-green-500" :name="'badge-check'" />
+      <Icon
+        class="inline-flex h-6 w-6 items-center text-green-500"
+        :name="'badge-check'"
+      />
       <span>{{ message }}</span>
     </span>
     <span v-if="response === 3">
-      <Icon class="text-yellow-500" :name="'information-circle'" />
+      <Icon
+        class="inline-flex h-6 w-6 items-center text-yellow-500"
+        :name="'information-circle'"
+      />
       <span>{{ message }}</span>
     </span>
   </div>
