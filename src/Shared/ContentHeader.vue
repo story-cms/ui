e05cb401afe1737324e723dc87bc3dfc00e354c7
Timothy Koech <timosville@gmail.com>
2023-11-30 14:06:46 +0300

    refactor: change title case and font

--- src/Shared/ContentHeader.vue
+++ src/Shared/ContentHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto">
     <div class="flex items-center justify-between py-6">
-      <h3 class="font-['Inter'] text-lg font-bold leading-7 text-gray-800">
+      <h3 class="font-['Inter'] text-2xl font-semibold text-gray-800">
         {{ title }}
       </h3>
       <div class="flex items-center space-x-6">
