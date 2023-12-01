299da036217d1438f44df05297b127d6db99eb8f
Jannie Theunissen <jannie@scoutredeem.co>
2023-12-01 10:21:22 +0200

    fix: resize observer

--- src/Shared/AppLayout.vue
+++ src/Shared/AppLayout.vue
@@ -1,27 +1,24 @@
 <template>
-  <div ref="app_layout" class="min-h-screen bg-app_gray">
+  <div class="min-h-screen bg-app_gray">
     <slot name="header"></slot>
     <slot></slot>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted } from 'vue';
 import { useSharedStore } from '../store';
 const shared = useSharedStore();
 
-const app_layout = ref<HTMLElement | null>(null);
-
-const resizeObserver = (element: HTMLElement) => {
-  const instance = new ResizeObserver((entries) => {
-    for (const entry of entries) {
-      const { width } = entry.contentRect;
-      shared.setLargeScreen(width >= 1116);
-    }
-  });
-  instance.observe(element);
+const resizeHook = () => {
+  const fresh = document.documentElement.clientWidth;
+  shared.setLargeScreen(fresh >= 1116);
 };
 
 onMounted(() => {
-  resizeObserver(app_layout.value as HTMLElement);
+  window.addEventListener('resize', resizeHook);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', resizeHook);
 });
 </script>
