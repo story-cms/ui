ba31e1870a85dfc8cfe427194053c51c41be3e62
lintoncaldecott <linton@caldecott.me>
2024-03-05 08:23:50 +0200

    feat: more workflow button story variants

--- src/Draft/WorkflowButtons.story.vue
+++ src/Draft/WorkflowButtons.story.vue
@@ -1,15 +1,33 @@
 <template>
   <Story title="Workflow Buttons" group="draft">
-    <Variant title="Admin no review" :setup-app="loadData">
+    <Variant title="Admin no review draft submitted" :setup-app="loadData">
       <WorkflowButtons />
     </Variant>
-    <Variant title="Admin has review" :setup-app="loadData">
+    <Variant title="Admin has review draft submitted" :setup-app="loadData">
       <WorkflowButtons />
     </Variant>
-    <Variant title="Editor has review" :setup-app="loadData">
+    <Variant title="Editor has review draft submitted" :setup-app="loadData">
       <WorkflowButtons />
     </Variant>
-    <Variant title="Editor no review" :setup-app="loadData">
+    <Variant title="Editor no review draft submitted" :setup-app="loadData">
+      <WorkflowButtons />
+    </Variant>
+    <Variant title="Admin no review draft started" :setup-app="loadData">
+      <WorkflowButtons />
+    </Variant>
+    <Variant title="Admin no review draft started" :setup-app="loadData">
+      <WorkflowButtons />
+    </Variant>
+    <Variant title="Admin has review draft started" :setup-app="loadData">
+      <WorkflowButtons />
+    </Variant>
+    <Variant title="Editor has review draft started" :setup-app="loadData">
+      <WorkflowButtons />
+    </Variant>
+    <Variant title="Editor no review draft started" :setup-app="loadData">
+      <WorkflowButtons />
+    </Variant>
+    <Variant title="Admin no review draft started" :setup-app="loadData">
       <WorkflowButtons />
     </Variant>
   </Story>
@@ -26,21 +44,44 @@ const loadData: Vue3StorySetupHandler = ({ variant }) => {
   const drafts = useDraftsStore();
 
   shared.setFromProps(sharedProps);
-  if (variant?.title == 'Admin no review') {
+  if (variant?.title == 'Admin no review draft submitted') {
     shared.meta.hasEditReview = false;
     shared.user.role = 'admin';
+    drafts.draft.status = 'submitted';
   }
-  if (variant?.title == 'Admin has review') {
+  if (variant?.title == 'Admin has review draft submitted') {
     shared.meta.hasEditReview = true;
     shared.user.role = 'admin';
     drafts.draft.status = 'submitted';
   }
-  if (variant?.title == 'Editor no review') {
+  if (variant?.title == 'Editor has review draft submitted') {
+    shared.meta.hasEditReview = true;
+    shared.user.role = 'editor';
+    drafts.draft.status = 'submitted';
+  }
+  if (variant?.title == 'Editor no review draft submitted') {
     shared.meta.hasEditReview = false;
     shared.user.role = 'editor';
+    drafts.draft.status = 'submitted';
   }
-  if (variant?.title == 'Editor has review') {
+
+  if (variant?.title == 'Admin no review draft started') {
+    shared.meta.hasEditReview = false;
+    shared.user.role = 'admin';
+    drafts.draft.status = 'started';
+  }
+  if (variant?.title == 'Admin has review draft started') {
     shared.meta.hasEditReview = true;
+    shared.user.role = 'admin';
+    drafts.draft.status = 'started';
+  }
+  if (variant?.title == 'Editor has review draft started') {
+    shared.meta.hasEditReview = true;
+    shared.user.role = 'editor';
+    drafts.draft.status = 'started';
+  }
+  if (variant?.title == 'Editor no review draft started') {
+    shared.meta.hasEditReview = false;
     shared.user.role = 'editor';
     drafts.draft.status = 'started';
   }
