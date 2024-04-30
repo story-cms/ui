8ecf5334d5ed147b65d421aed68aff3450015687
Jannie Theunissen <jannie@scoutredeem.co>
2024-04-30 16:53:51 +0200

    refactor: language picker width the language picker dropdown now has dynamic width for selecting long language names

--- src/Shared/DropDown.story.vue
+++ src/Shared/DropDown.story.vue
@@ -3,6 +3,9 @@
     <Variant title="With options">
       <DropDown v-model="model" :options="options" @change="onSet" />
     </Variant>
+    <Variant title="With long language names">
+      <DropDown v-model="language" :options="languages" @change="onLanguage" />
+    </Variant>
     <Variant title="Read only">
       <DropDown v-model="model" :options="options" :is-read-only="true" />
     </Variant>
@@ -15,8 +18,39 @@ import { ref } from 'vue';
 
 const options = ['Add', 'Edit', 'Delete'];
 const model = ref('Edit');
-
 const onSet = async (val: string) => {
   model.value = val;
+};
+
+const language = ref('French - Français');
+const languages = [
+  'English',
+  'Albanian - Shqipe',
+  'Arabic - عربى',
+  'Bengali - বাংলা',
+  'Chinese - 中文',
+  'French - Français',
+  'Fulah Adamawa – Fulfulde Adamawa',
+  'Georgian – ქართული',
+  'German - Deutsch',
+  'Hausa',
+  'Indonesian - Bahasa Indonesia',
+  'Italian - Italiano',
+  'Kurdish Sorani – کوردی سۆرانی',
+  'Kurmanji - Kurmancî',
+  'Persian - فارسی',
+  'Portuguese - Português',
+  'Russian – Русский',
+  'Somali - Af Soomaali',
+  'Spanish - Español',
+  'Swahili – Kiswahili',
+  'Tachelhite - Taclḥiyt',
+  'Thai - ภาษาไทย',
+  'Turkish - Türkçe',
+  'Urdu - اُردو',
+  'Uzbek – أۇزبېك ﺗﻴﻠی',
+];
+const onLanguage = async (val: string) => {
+  language.value = val;
 };
 </script>
